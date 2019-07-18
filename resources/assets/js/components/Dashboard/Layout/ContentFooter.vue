a2b5bdc124e250e1b8a49003d5e0d4e671c7a2af
Sithiphone <sithiphone@fe-nuol.edu.la>
2019-07-18 14:50:36 +0700

    update contents

--- resources/assets/js/components/Dashboard/Layout/ContentFooter.vue
+++ resources/assets/js/components/Dashboard/Layout/ContentFooter.vue
@@ -11,8 +11,8 @@
       <div class="copyright text-center">
         &copy; Coded with
         <i class="fa fa-heart heart"></i> by
-        <a href="https://github.com/cristijora" target="_blank">Cristi Jora</a>.
-        Designed by <a href="https://www.creative-tim.com/?ref=pdf-vuejs" target="_blank">Creative Tim</a>.
+        <a href="http://fe-nuol.edu.la/ceit" target="_blank">SOFTWARE DEV</a>.
+        Designed by <a href="http://fe-nuol.edu.la/ceit" target="_blank">Software team</a>.
       </div>
     </div>
   </footer>
